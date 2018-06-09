472299635daea51450cdf31029a97b1c1a799da3
Stimons <stijn.simons2@gmail.com>
2018-06-09 16:32:00 +0200

    Beginning of Packet handler

---
+++ src/main/java/xyz/baddeveloper/lwsl/packet/Packet.java
@@ -0,0 +1,11 @@
+package xyz.baddeveloper.lwsl.packet;
+
+import org.json.JSONObject;
+
+public class Packet {
+    private JSONObject output;
+
+    public JSONObject getOutput() {
+        return output;
+    }
+}
