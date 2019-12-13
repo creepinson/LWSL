428498a7f671531607491b20c23a766ca8d84845
Baddeveloper <stijn.simons2@gmail.com>
2019-12-13 16:37:55 +0100

    Remove unused imports

--- src/main/java/xyz/baddeveloper/lwsl/client/events/OnPacketSentEvent.java
+++ src/main/java/xyz/baddeveloper/lwsl/client/events/OnPacketSentEvent.java
@@ -2,7 +2,6 @@ package xyz.baddeveloper.lwsl.client.events;
 
 import xyz.baddeveloper.lwsl.client.SocketClient;
 import xyz.baddeveloper.lwsl.packet.Packet;
-import xyz.baddeveloper.lwsl.server.SocketHandler;
 
 public interface OnPacketSentEvent {
 
